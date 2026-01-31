01d5c13bdf1c0dc37d98a1264ba27a0fc133b860
xiao_wine <68009112+xiaowine@users.noreply.github.com>
2026-01-31 22:16:39 +0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# 1.1.1
+
+## 变更
+
+1. 为了符合隐私政策，禁止在 extension.json、README.md、CHANGELOG.md、LICENSE 内添加电子邮箱地址作为联系方式
+
+# 1.1.0
+
+## 新增
+
+1. 新增扩展注册头部菜单的多语言翻译支持
+2. 新增更新日志（CHANGELOG.md）
+
+## 变更
+
+1. 替换已弃用的方法（SYS_Dialog.showInformationMessage）
+
+# 1.0.0
+
+初始版本
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/**
+ * 入口文件
+ *
+ * 本文件为默认扩展入口文件，如果你想要配置其它文件作为入口文件，
+ * 请修改 `extension.json` 中的 `entry` 字段；
+ *
+ * 请在此处使用 `export`  导出所有你希望在 `headerMenus` 中引用的方法，
+ * 方法通过方法名与 `headerMenus` 关联。
+ *
+ * 如需了解更多开发细节，请阅读：
+ * https://prodocs.lceda.cn/cn/api/guide/
+ */
+import * as extensionConfig from '../extension.json';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(status?: 'onStartupFinished', arg?: string): void {}
+
+const openPage = async (name: string, width: number, height: number) => {
+	const config = await eda.sys_Storage.setExtensionAllUserConfigs({ [extensionConfig.name]: name });
+	if (config) {
+		eda.sys_IFrame.openIFrame('/vue-dist/index.html', width, height, name);
+	}
+};
+
+export async function about(): Promise<void> {
+	await openPage('First', 400, 300);
+}
